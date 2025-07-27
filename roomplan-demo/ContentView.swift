d36bde6435029f979f5f7160959f9828b4a106cc
Dohyeop Lim <dhlim0817@gmail.com>
2025-07-28 01:21:27 +0900

    chore: project init

---
+++ roomplan-demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  roomplan-demo
+//
+//  Created by dohyeoplim on 7/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ roomplan-demo/roomplan_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  roomplan_demoApp.swift
+//  roomplan-demo
+//
+//  Created by dohyeoplim on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct roomplan_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
